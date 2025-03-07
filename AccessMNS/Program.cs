9d25e3657ef08c0ae2d5385c608e82bb0ead21f7
WEIN.A <alex.wein@hotmail.fr>
2025-03-07 15:28:40 +0100

    Restore AccessMNS

---
+++ AccessMNS/Program.cs
@@ -0,0 +1,43 @@
+using AccessMNS.Components;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using MudBlazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = false;
+});
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ AccessMNS/wwwroot/JavaScript/script.js
@@ -0,0 +1,29 @@
+﻿function saveScrollPosition(chatId) {
+    const chatContainer = document.getElementById(chatId);
+    if (chatContainer) {
+        sessionStorage.setItem("scroll_" + chatId, chatContainer.scrollTop);
+    }
+}
+
+function restoreScrollPosition(chatId) {
+    let chatContainer = document.getElementById(chatId);
+    let savedScroll = sessionStorage.getItem("scroll_" + chatId);
+    if (chatContainer && savedScroll) {
+        chatContainer.scrollTop = savedScroll;
+    }
+}
+
+function scrollToBottom(chatId) {
+    let chatContainer = document.getElementById(chatId);
+    if (chatContainer) {
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+}
+
+function isAtBottom(chatId) {
+    let chatContainer = document.getElementById(chatId);
+    if (chatContainer) {
+        return chatContainer.scrollHeight - chatContainer.scrollTop <= chatContainer.clientHeight + 10;
+    }
+    return false;
+}
